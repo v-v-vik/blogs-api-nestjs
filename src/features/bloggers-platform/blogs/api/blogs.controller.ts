b9406a0bf0f23174375c9c2b722f1796cd7cb64d
Vera Vikhreva <vvikhreva.wp@gmail.com>
2025-01-04 19:07:46 +0100

    add some endpoints for blogs controller

--- src/features/bloggers-platform/blogs/api/blogs.controller.ts
+++ src/features/bloggers-platform/blogs/api/blogs.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { BlogsService } from '../application/blogs.service';
+import { PaginatedViewDto } from '../../../../core/dto/base.paginated.view-dto';
+import { BlogViewModel, CreateBlogModel } from '../domain/dto/blog.models';
 
 @Controller('blogs')
-export class BlogsController {}
+export class BlogsController {
+  constructor(
+    private blogsQueryRepository: BlogsQueryRepository,
+    private blogsService: BlogsService,
+  ) {}
+
+  @Get()
+  async findAll(
+    @Query() query: GetBlogsQueryParams,
+  ): Promise<PaginatedViewDto<BlogViewModel[]>> {
+    return this.blogsQueryRepository.findAll(query);
+  }
+
+  @Get('id')
+  async findById(@Param('id') id: string): Promise<BlogViewModel> {
+    return this.blogsQueryRepository.getByIdOrNotFoundFail(id);
+  }
+
+  @Post()
+  async create(@Body() body: CreateBlogModel): Promise<BlogViewModel> {
+    const blogId = await this.blogsService.create(body);
+    return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
+  }
+}
